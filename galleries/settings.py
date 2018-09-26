d8a1c36a36357c1aed925f5a95ca28df829a19ef
Nathaniel Ott Homer <nathaniel.ott.homer@gmail.com>
2018-09-25 23:24:36 -0400

    hook up django-webpack-loader

--- galleries/settings.py
+++ galleries/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'galleries'
+    'galleries',
+    'webpack_loader'
 ]
 
 MIDDLEWARE = [
@@ -117,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/dist/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'dist'),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require("path");
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+  context: __dirname,
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist/"),
+    publicPath: "/dist/",
+    filename: "[name]-[hash].js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { 
+    extensions: ["*", ".js", ".jsx"]
+  },
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'}),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
