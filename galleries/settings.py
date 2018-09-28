9233673766b8da3d4aff96e33338c02532cd4868
Nathaniel Ott Homer <nathaniel.ott.homer@gmail.com>
2018-09-27 22:05:48 -0400

    add psycopg2@2.7.5

--- galleries/settings.py
+++ galleries/settings.py
@@ -82,10 +82,15 @@ WSGI_APPLICATION = 'galleries.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-      'default': dj_database_url.config(
-          default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-      )
-  }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'galleries',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}  
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
